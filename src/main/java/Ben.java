1010255b3350785d763b205bb925fed835b8df3f
Ruben <xcreeper0511@gmail.com>
2026-01-23 08:50:11 +0800

    Level-0: Rename, Greet, Exit

---
+++ src/main/java/Ben.java
@@ -0,0 +1,11 @@
+public class Ben {
+    public static void main(String[] args) {
+        String horizontal_lines = "----------------------------------------";
+        System.out.println(horizontal_lines);
+        System.out.println("Hello! I'm Ben");
+        System.out.println("What can I do for you?");
+        System.out.println(horizontal_lines);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(horizontal_lines);
+    }
+}
