c39ba6d2865dd44ae8a232af8d26f9f263506e5f
Ruben <xcreeper0511@gmail.com>
2026-01-30 14:28:33 +0800

    Level-8: Date and Times

--- src/main/java/ben/Deadline.java
+++ src/main/java/ben/Deadline.java
@@ -1,30 +1,41 @@
 package ben;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 /**
  * Represents a task with a specified deadline
  *
  */
 public class Deadline extends Task{
-    private String date;
+    private LocalDate due;
+
+    private static final DateTimeFormatter INPUT_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    private static final DateTimeFormatter OUTPUT_FORMAT = DateTimeFormatter.ofPattern("MMM dd yyyy");
 
     /**
-     * Creates a deadline task with a specified deadline and description.
+     * Creates a deadline task with a specified string deadline and description.
      *
-     * @param date Due date of the task.
-     * @param description Description of the task.
+     * @param date String due date of the task.
+     * @param description String description of the task.
      */
-    public Deadline(String date, String description){
+    public Deadline(String date, String description) throws BenException{
         super(description);
-        this.date = date;
+        try {
+            this.due = LocalDate.parse(date, INPUT_FORMAT);
+        } catch (Exception e) {
+            throw new BenException("Invalid date format. Please follow dd/MM/yyyy");
+        }
     }
 
     @Override
     public String returnStatus(){
-        return "[D]" + "[" + super.getStatusIcon() + "] " + super.getDescription() + " (by: " + date + ")";
+        return "[D]" + "[" + super.getStatusIcon() + "] " + super.getDescription() + " (by: " + due.format(OUTPUT_FORMAT) + ")";
     }
 
     @Override
     public String fromTaskToString(){
-        return "D | " + (super.isDone() ? 1 : 0) + " | " + super.getDescription() + " | " + date;
+        // Save to file data using input format and only use output format for printing.
+        return "D | " + (super.isDone() ? 1 : 0) + " | " + super.getDescription() + " | " + due.format(INPUT_FORMAT);
     }
 }
